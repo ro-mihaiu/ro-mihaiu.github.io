343bec02e26b600cd9b972def97890b4a291cb94
Mihaiu <mihaicpm75@gmail.com>
2026-06-10 21:27:04 +0300

    styling guides #2

--- src/data/warps.ts
+++ src/data/warps.ts
@@ -14,7 +14,7 @@ export const warps: Warp[] = [
     name: 'mihu-farm',
     status: 'active',
     desc: 'Mega farm for crops and resources - maintained regularly.',
-    guide: `## Rules
+    guide: '',
     items: [
       { name: 'Enchanted Decree', icon: '/assets/items/enchanted-decree.png' },
       { name: 'Trials of Olympus', icon: '/assets/items/trials-of-olympus.png' },
@@ -42,67 +42,7 @@ export const warps: Warp[] = [
         text: "Don't break anything except the crops and always replant them",
         note: "Make sure to use a hoe/item that auto-replants; Demeters Touch is available in the basement."
       }
-    ],
-- Any tool you took from the Player Warp, must be returned. Don't log off with the items.
-> Unless you have serious reasons for keeping it
-- In case you don't need the goods, they can be donated on the 9th floor at pw mihu-farm.
-> Goods, such as carrots/potatos/wheat/seeds/etc
-- Being a discord member of my server is a *must*, in order to keep track of who is trusted
-> Anyone from this server can promote it to their friends, and may get a reward
-- Don't break anything except the crops and after that **replant them always**
-> Make sure to use a hoe/item that auto-replants, since it will make your work easier (I have put a Demeters Touch in the basement)
-- Any suggestions are welcome
-> For auto-farms / manual farms / design, Make them at ⁠plans-suggestions(https://discord.com/channels/1263915963464548483/1493313863364644966)
-- Any donations are welcome, you will be placed on a wall of fame
-- You will need at least **Ranger** rank to be verified
-> Mostly because I don't want this farm to be griefed
-- When renting/borrowing an item you must put your head in the barrel
-> Do /hdb search (user) to get yourself your head
-- Copying my builds/property in any way will get your reported to BlossomCraft staff
-> Discord server's template / rules are included
-
-The rules can be updated anytime, it is up to you to check and read them
-
-## Items
-- Enchanted Decree
-> For woodcutting quests
-- Tryals of Olympus
-> For any type of quests
-- Mistweave
-> For breaking leaves faster
-- Deidara Shovel
-> For getting clay instead of soul sand/soil [RW]
-- Void Shovel
-> For stocking dirt, so your inventory is clear
-- Whisperwood
-> For getting the logs you want & chop down trees faster
-- Atlas Axe
-> For chopping down trees/mushrooms faster
-- Kodama
-> For bonemealing crops/trees faster
-- Mystical Sickle
-> For tilling dirt faster
-- Demeters Touch
-> For replanting easier [pls have a clear inv, before using]
-- Sylvan Staff
-> For placing tree saplings faster
-- Shulkers
-> Don't take them all, only if u wanna donate crops to F9
-
-## Layout
-F0 - Entry / SpawnPoint
-F1 - Wheat
-F2 - Carrots
-F3 - Potatos
-F4 - Beetroots
-F5 - Wheat + Potatos
-F6 - Carrots
-F7 - Nether Wart
-F8 - Automatic Farms
-F9 - Lunge (Future SpawnPoint)
-F10 - Spruce Trees
-F11 - Mob Farms
-'F' as of floor`,
+    ]
   },
   { id: 'mihu-rentals', name: 'mihu-rentals', status: 'active', desc: 'Item rentals, mostly tools and blocks - follow posted rules.', guide: 'Rentals are managed through signs. Respect expiry times and return rented items in good condition.' },
   { id: 'mihu-shop', name: 'mihu-shop', status: 'upcoming', desc: 'Community shop + Blossom Items - opening soon with trades.', guide: 'Shop will support currency and item trades. Check announcement boards for opening date.' },
